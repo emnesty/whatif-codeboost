eabeb05ae347d92993baa1e79f2914fb9d83a489
Luciano Silva <legostr@gmail.com>
2023-04-11 20:00:01 -0300

    Novas secoes da home

---
+++ src/components/home/sectionHero/styles.js
@@ -0,0 +1,12 @@
+// This code is used to style the section of the page with the hero image.
+// The section is styled using the "SectionHeroStyle" class.
+// The area with the social media icons is styled using the "AreaSocial" class.
+// The area with the text is styled using the "ContextText" class.
+
+import { styled } from "../../../styles";
+
+export const SectionHeroStyle = styled("section", {});
+
+export const AreaSocial = styled("div", {});
+
+export const ContextText = styled("div", {});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import { Header } from "../components/header";
-
+import { SectionHero } from "../components/home/sectionHero";
 
 export default function Home() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <SectionHero />
+    </>
+  );
 }
