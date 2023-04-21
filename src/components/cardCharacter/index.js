543d6dd673c1b136e034ece170dc131739daf9fc
Luciano Silva <legostr@gmail.com>
2023-04-20 21:58:43 -0300

    Seção heroes

--- src/components/cardCharacter/index.js
+++ src/components/cardCharacter/index.js
@@ -1,11 +1,28 @@
 import Link from "next/link"
 import { StyleCard } from "./styles"
 
+import ImageCharacter from "../../assets/character.jpg"
+import Image from "next/image"
+
 export function CardCharacter() {
   return (
     <StyleCard>
       <Link href="#" legacyBehavior>
-        <a>About Us</a>
+        <a>
+          <div className="image">
+            <Image 
+            src={ImageCharacter} 
+            alt="Character" 
+            />
+          </div>
+          <div className="info">
+            <div>
+              <h3>T'Challa</h3>
+              <span>Marvel Studios</span>
+            </div>
+            <span>What if</span>
+          </div>
+        </a>
       </Link>
     </StyleCard>
   )
--- src/components/cardCharacter/styles.js
+++ src/components/cardCharacter/styles.js
@@ -1,3 +1,14 @@
 import { styled } from "../../styles"
 
-export const StyleCard = styled("div", {})
+export const StyleCard = styled("div", {
+    width: "100%",
+    maxWidth: "28rem",
+    a: {
+        display: "block",
+    },
+    "&.image": {
+        width: "100%",
+        height: "372px",
+        marginBottom: 16,
+    }
+})
